74d5c96839fd4524b5cdc14a73a86938041a0170
Vedansh Patel <vcp23062006@gmail.com>
2025-11-24 21:44:40 +0530

    springboot

---
+++ routingSimulator/src/test/java/com/example/routingSimulator/RoutingSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.routingSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
