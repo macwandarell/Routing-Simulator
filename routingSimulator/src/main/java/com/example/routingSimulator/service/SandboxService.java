9c1d1e19f06847c9a4e127e2e5e33f17085b95fc
vedant010107 <vedant010107@gmail.com>
2025-11-25 15:56:14 +0530

    medium change(manager)

--- routingSimulator/src/main/java/com/example/routingSimulator/service/SandboxService.java
+++ routingSimulator/src/main/java/com/example/routingSimulator/service/SandboxService.java
@@ -154,8 +154,8 @@ public class SandboxService {
 
         sb.append("</div>");
 
-        GlobeManager globemanager= sandboxRegistry.get(id);
-        sb.append(globemanager.printView());
+        GlobeManager globeManager= sandboxRegistry.get(id);
+        sb.append(globeManager.printView());
         return sb.toString();
     }
     public String updateSandbox(int id, String json) {
@@ -163,12 +163,16 @@ public class SandboxService {
             JsonNode root = objectMapper.readTree(json);
             GlobeManager globeManager = sandboxRegistry.get(id);
 
+
+
+
             if (globeManager == null) {
                 return "Sandbox with ID " + id + " not found.";
             }
             String responseMessage = handleCommands(globeManager, root);
-            StringBuilder sb = new StringBuilder();
 
+
+            StringBuilder sb = new StringBuilder();
             sb.append("<div style=\""
                     + "background-image: url('/banners/background.jpg'); "
                     + "background-size: cover; "
@@ -183,22 +187,39 @@ public class SandboxService {
                     + "background-color: rgba(0,0,0,0.5); "
                     + "overflow-y: auto;"
                     + "\">");
-            sb.append("<h2 style='color:cyan;'>Sandbox #" + id + "</h2>");
-            sb.append("<p style='color:yellow;'>Last action: " + responseMessage + "</p><br>");
-            sb.append("<form method='POST' action='/play/sandbox/" + id + "' "
-                    + "style='display:flex;flex-direction:column;max-width:400px;'>");
-            sb.append("<textarea name='json' placeholder='Enter command JSON here...' "
-                    + "style='height:150px;width:100%;background:black;color:white;"
-                    + "border:1px solid gray;padding:10px;'></textarea>");
-            sb.append("<button type='submit' "
-                    + "style='margin-top:10px;padding:10px;background:darkred;border:none;color:white;'>Apply Update</button>");
-            sb.append("</form>");
-            sb.append("<br><a href='/play' style='color:red;text-decoration:none;'>Back to Playground</a>");
+
+            // Load logo.txt from classpath
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(getClass().getResourceAsStream("/banners/sandbox.txt")))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("<br>");
+                }
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+                sb.append("Error loading welcome message.<br>");
+            }
+
+            // Commands section
+            sb.append("<br>");
+            sb.append("<span style='color:yellow;'>Here is a list of the available commands (case doesn't matter)</span><br>");
+            sb.append("<a href='/play/sandbox/").append(id).append("' style='color:red;text-decoration:none;'>/play/sandbox/").append(id).append("</a>").append("&nbsp;&nbsp;<span style='color:yellow;'>- The current page you are in</span><br>");
             sb.append("<br><hr style='border-color:gray;'><br>");
-            sb.append("<div>");
+            sb.append("<h3 style='color:cyan;'>Create new manager in this Sandbox</h3>");
+            sb.append("<form method='POST' action='/play/sandbox/").append(id).append("' " +
+                    "style='display:flex;flex-direction:column;max-width:400px;'>");
+            sb.append("<textarea name='json' placeholder='Enter JSON here...' " +
+                    "style='height:150px;width:100%;background:black;color:white;border:1px solid gray;padding:10px;'></textarea>");
+            sb.append("<button type='submit' " +
+                    "style='margin-top:10px;padding:10px;background:darkred;border:none;color:white;'>Create new manager</button>");
+            sb.append("</form>");
+            sb.append("<a href='/play' style='color:red;text-decoration:none;'>/play</a>").append("&nbsp;&nbsp;<span style='color:yellow;'>- goes back to the playground page</span><br>");
+            sb.append("<p style='color:yellow;'>Last action: " + responseMessage + "</p><br>");
+
+            sb.append("</div>");
             sb.append(globeManager.printView());
-            sb.append("</div>");
-            sb.append("</div>");
+
+
             return sb.toString();
 
         } catch (Exception e) {
